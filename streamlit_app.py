e0aaf18016a71aa362900fde502cac322a34ec0c
kashimurayoshiko-oss <kashimurayoshiko@gmail.com>
2025-09-09 13:10:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.write("最初の第一歩ですかね")
+st.write("最初のヨチヨチ第一歩ですかね")
